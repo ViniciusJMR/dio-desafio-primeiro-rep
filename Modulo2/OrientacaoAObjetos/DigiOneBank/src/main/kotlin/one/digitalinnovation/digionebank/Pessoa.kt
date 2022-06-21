635fa3a556cb6e73c1441caa8c51277656c55cf7
Vinicius Rodrigues <viniciusrodrigues@pop-os.localdomain>
2022-06-21 18:01:04 -0300

    Orientação a objetos: Aplicado o modificador da função set para o cpf de Pessoa

--- Modulo2/OrientacaoAObjetos/DigiOneBank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ Modulo2/OrientacaoAObjetos/DigiOneBank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,6 +3,7 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     val nome: String = "Jether"
     var cpf: String = "123.123.123-11"
+    private set
 
     inner class Endereco {
         var rua: String = "Rua teste"
