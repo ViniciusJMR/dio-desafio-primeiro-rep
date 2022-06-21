e2c6da75c292014449514012443b563c42783a37
Vinicius Rodrigues <viniciusrodrigues@pop-os.localdomain>
2022-06-21 17:48:57 -0300

    Orientação a objetos: Exemplo Inner Class

--- Modulo2/OrientacaoAObjetos/DigiOneBank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ Modulo2/OrientacaoAObjetos/DigiOneBank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,10 +4,15 @@ class Pessoa {
     val nome: String = "Jether"
     var cpf: String = "123.123.123-11"
 
+    inner class Endereco {
+        var rua: String = "Rua teste"
+    }
+
 }
 
 fun main(){
     val p = Pessoa()
 
     println("Nome: ${p.nome}; cpf: ${p.cpf}")
+    println(p.Endereco().rua )
 }
