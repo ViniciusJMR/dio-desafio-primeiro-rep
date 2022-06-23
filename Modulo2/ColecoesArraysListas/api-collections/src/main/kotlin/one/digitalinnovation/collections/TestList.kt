6c3e9f0e6f4d8d6c11e48f910cf28e443fd09f72
Vinicius Rodrigues <viniciusrodrigues@pop-os.localdomain>
2022-06-22 21:20:57 -0300

    Coleções Arrays e Listas: Criado a testeList

---
+++ Modulo2/ColecoesArraysListas/api-collections/src/main/kotlin/one/digitalinnovation/collections/TestList.kt
@@ -0,0 +1,35 @@
+package one.digitalinnovation.collections
+
+fun main(){
+    val joao = Funcionario("Joao", 9880.0, "CLT")
+    val maria = Funcionario("Maria", 3040.0, "CLT")
+    val pedro = Funcionario("Pedro", 4000.0, "Pj")
+
+    val funcionarios = arrayListOf(joao, pedro, maria)
+
+    funcionarios.forEach{ println(it) }
+
+    println("=================")
+    println(funcionarios.find{it.nome == "Maria"})
+
+    println("=================")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach{ println(it) }
+
+    println("=================")
+    funcionarios
+        .groupBy{it.tipo}
+        .forEach{println(it)}
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipo: String
+){
+    override fun toString() = """
+        Nome:       $nome
+        salario:    $salario
+    """.trimIndent()
+}
