b09edabde454aaabf754554639e8c2f2a0692c8e
Vinicius Rodrigues <viniciusrodrigues@pop-os.localdomain>
2022-07-08 15:46:25 -0300

    MatchesSimulatorApp: Criado a tela de Detalhe

---
+++ Modulo2/MatchesSimulatorApp/app/src/main/java/me/dio/matchessimulatorapp/DetailActivity.kt
@@ -0,0 +1,19 @@
+package me.dio.matchessimulatorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.dio.matchessimulatorapp.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
